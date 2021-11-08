6bf3445c2e0e1e14e6ee486d36734d4004af737d
XuyenHoang <xhoang21@gmail.com>
2021-11-08 18:48:36 -0500

    still a long ways to go

--- app.js
+++ app.js
@@ -1,89 +1,150 @@
 var questionsarr = [
      {
-         question:  "What city and state was McDonald's founded in?",
-         Answers: { A: "San Bernardino, CA", 
+         question: "What city and state was McDonald's founded in?",
+        Answers: { A: "San Bernardino, CA", 
                      B: "Dallas, TX",
                      C: "Tampa, FL",
                      D: "Long Beach, CA"
         },
-     
-         question: "what country dose wagyu orginated from?",
-         Answers: { A: "America", 
+        correct: "0"
+    },
+    {
+          question: "what country dose wagyu orginated from?",
+        Answers: { A: "America", 
                      B: "China",
                      C: "France",
                      D: "Japan"
         },
-     
-        question:  "What city and state was McDonald's founded in?",
+        correct: "3"
+    },
+    { 
+        question: "What city and state was churchs chicken founded in?",
         Answers: { A: "San Bernardino, CA", 
                     B: "Dallas, TX",
                     C: "Tampa, FL",
                     D: "Long Beach, CA"
-                },
-        
-       question:  "What city and state was McDonald's founded in?",
-        Answers: { A: "San Bernardino, CA", 
-                    B: "Dallas, TX",
-                    C: "Tampa, FL",
-                    D: "Long Beach, CA"
-       },
-       question:  "What city and state was McDonald's founded in?",
-        Answers: { A: "San Bernardino, CA", 
-                    B: "Dallas, TX",
-                    C: "Tampa, FL",
-                    D: "Long Beach, CA"
-       },
-       question:  "What city and state was McDonald's founded in?",
+        },
+        correct: "2"
+    },
+    {
+        question:"What city and state was white castle founded in?",
         Answers: { A: "San Bernardino, CA", 
                     B: "Dallas, TX",
                     C: "Tampa, FL",
                     D: "Long Beach, CA"
        },
-    }];
-var correctAnsr = ["a","D"]
-
-
-let shuffleQue =[]
-
-
-function QueLineUp(){
-    while (shuffleQue.length <= 6){
-        const random = question[Math.floor(Math.random()) * question.length]
-        console.log(random)
-        if (!shuffleQue.includes(random)) {
-            shuffleQue.push(random) 
-            document.getElementById("Question").innerText = shuffleQue.push(random)
-        }
+       correct: "2"
+    },
+    {
+        question: "What city and state was KFC founded in?",
+        Answers: { A: "San Bernardino, CA", 
+                    B: "Dallas, TX",
+                    C: "Tampa, FL",
+                    D: "Long Beach, CA"
+       },
+       correct: "1"
+    },
+    {
+        question: "What city and state was burger king founded in?",
+       Answers: { A: "San Bernardino, CA", 
+                    B: "Dallas, TX",
+                    C: "Tampa, FL",
+                    D: "Long Beach, CA"
+       },
+       correct: "3"
     }
-}
-// const queLine =  = 
-const queLine = document.getElementById("Question").innerText = questionsarr[0].question;
+    ];
+// let correctAnsr = ["a","D"]
+// const queLine = document.getElementById("Question").innerText = questionsarr[0].question1;
+
+
+
+
+const AnsrLineUp = document.querySelectorAll(".answer")
+const queLine = document.getElementById("Question")
+let shuffleQue =[]
+function QueLineUp(){
+    while (shuffleQue.length < 6){ 
+        let random = questionsarr[Math.floor(Math.random() * questionsarr.length)]
+        if (!shuffleQue.includes(random)) {
+            shuffleQue.push(random)
+        }
+        }
+    console.log(shuffleQue)
+    return shuffleQue 
+}        
+
+// queLine.innerHTML = shuffleLineup[0][0]
+// console.log(QueLineUp)
+QueLineUp()
+queLine.innerHTML = shuffleQue[0].question
+console.log(shuffleQue[0].question)
+
+const answerButton = [
+    document.getElementById("A").innerText = shuffleQue[0].Answers.A,
+    document.getElementById("B").innerText = shuffleQue[0].Answers.B,
+    document.getElementById("C").innerText = shuffleQue[0].Answers.C,
+    document.getElementById("D").innerText = shuffleQue[0].Answers.D,
+]
+console.log(answerButton)
+
+function ButtonAswr (){
+       if (shuffleQue[0].correct = answerButton){
+            document.querySelector("#feedback").innerHTML = "<h3>Correct!</h3>";
+        } else document.querySelector("#feedback").innerHTML = "<h3>Wrong!</h3>"
+        console.log(answerButton.value)
+}    
+answerButton.addEventListener("click",ButtonAswr)
+
+const NextBtn = document.querySelector(".next")
+// function NextQueLine
+// for (let i=0; i < answers.length; i++){
+        //         answers[i] = AnsrLineUp;
+        //         console.log(answers[i])
+        //         AnsrLineUp.innerText = shuffleLineup[0].Answers[i]
+        // }
+
+
+
+
+// function ansrLine(){
+//     for (let i=0; i < answers.length; i++){
+//         answers[i] = AnsrLineUp;
+//         console.log(answers[i])
+//         AnsrLineUp.innerText = shuffleLineup[0].Answers[i]
+// }
+// }
+// console.log(ansrLine)
+
+// }
+// console.log(TraiviaAnswer)
+// console.log(questionsarr[0].Answers)
+
+// if( questionsarr.Answers === correctAnsr[1]){
+//     correctAnsr++;
+//     questionsarr.Answers.style.WinHighlight
+// }else{
+//     questionsarr.Answers.style.LoseHighlight
+// }
+
+
+
+
 // console.log(queLine,"line 25")
 // console.log(questionsarr[0].question)
 // const butanswers= document.querySelectorAll("answers")
 // answerButton.forEach(butanswers => butanswers.addEventLister("click",ButtonAswr))
-const answerButton = [
-         document.getElementById("A").innerText = questionsarr[0].Answers.A,
-         document.getElementById("B").innerText = questionsarr[0].Answers.B,
-         document.getElementById("C").innerText = questionsarr[0].Answers.C,
-         document.getElementById("D").innerText = questionsarr[0].Answers.D,
-]
-console.log(answerButton[0])
+// const answerButton = [
+//          document.getElementById("A").innerText = shuffleLineup.Answers.A,
+//          document.getElementById("B").innerText = questionsarr[0].Answers.B,
+//          document.getElementById("C").innerText = questionsarr[0].Answers.C,
+//          document.getElementById("D").innerText = questionsarr[0].Answers.D,
+// ]
+// console.log(answerButton[0])
 
 
-function ButtonAswr (){
-    for (let i=0; i < answerButton.length; i++);
-    for (let j=0; j < correctAnsr.length; j++);
-       if (answerButton[i] = correctAnsr[j]){
-            document.querySelector("#feedback").innerHTML = "<h3>Correct!</h3>";
-        } else document.querySelector("#feedback").innerHTML = "<h3>Wrong!</h3>"
 
-}
-console.log(ButtonAswr)
 
-        
-       
-
-    
-
+// }
+// console.log(ButtonAswr)
 
