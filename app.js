793182f1a0f3efff5fe3daf3159660c4a314f003
XuyenHoang <xhoang21@gmail.com>
2021-11-09 14:58:31 -0500

    added a score to the game

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// questions for the trivia
 var questionsarr = [
      {
          question: "What city and state was McDonald's founded in?",
@@ -54,14 +55,12 @@ var questionsarr = [
        correct: "3"
     }
     ];
-// let correctAnsr = ["a","D"]
+
 // const queLine = document.getElementById("Question").innerText = questionsarr[0].question1;
-
-
-
 
 const AnsrLineUp = document.querySelectorAll(".answer")
 const queLine = document.getElementById("Question")
+// this function is for shuffling the questions 
 let shuffleQue =[]
 function QueLineUp(){
     while (shuffleQue.length < 6){ 
@@ -74,38 +73,58 @@ function QueLineUp(){
     return shuffleQue 
 }        
 
-// queLine.innerHTML = shuffleLineup[0][0]
-// console.log(QueLineUp)
+
 QueLineUp()
 queLine.innerHTML = shuffleQue[0].question
 console.log(shuffleQue[0].question)
+// assinging the answers to a button
+document.getElementById("A").innerText = shuffleQue[0].Answers.A
+document.getElementById("B").innerText = shuffleQue[0].Answers.B
+document.getElementById("C").innerText = shuffleQue[0].Answers.C
+document.getElementById("D").innerText = shuffleQue[0].Answers.D
 
-const answerButton = [
-    document.getElementById("A").innerText = shuffleQue[0].Answers.A,
-    document.getElementById("B").innerText = shuffleQue[0].Answers.B,
-    document.getElementById("C").innerText = shuffleQue[0].Answers.C,
-    document.getElementById("D").innerText = shuffleQue[0].Answers.D,
+const answerButtons = [
+    document.getElementById("A"),
+    document.getElementById("B"),
+    document.getElementById("C"),
+    document.getElementById("D")
 ]
-console.log(answerButton)
+// this function is for giving a feedback for correct/wrong answers when clicked.
+function ButtonAswr (butnIdex){     
+    console.log(butnIdex)
+    if (parseInt(shuffleQue[0].correct) == butnIdex){
+         document.querySelector("#feedback").innerHTML = "<h3>Correct!</h3>";
+        parsedscore = parseInt(score.innerHTML)
+        parsedscore += 10
+        score.innerText = parsedscore.toString()
+        style.WinHighlight = "background-color: Green"
+    } else {
+        document.querySelector("#feedback").innerHTML = "<h3>Wrong!</h3>"
+        
+    }
+};
 
-function ButtonAswr (){
-       if (shuffleQue[0].correct = answerButton){
-            document.querySelector("#feedback").innerHTML = "<h3>Correct!</h3>";
-        } else document.querySelector("#feedback").innerHTML = "<h3>Wrong!</h3>"
-        console.log(answerButton.value)
-}    
-answerButton.addEventListener("click",ButtonAswr)
-
+answerButtons.forEach( (item,i) => {
+       item.addEventListener("click",() => ButtonAswr(i));   
+   }   
+)
+ 
 const NextBtn = document.querySelector(".next")
+const score = document.querySelector("#playerScore")
+
+NextBtn.innerHTML = "Next question!"
+
+
+
+
+
+
 // function NextQueLine
 // for (let i=0; i < answers.length; i++){
         //         answers[i] = AnsrLineUp;
         //         console.log(answers[i])
         //         AnsrLineUp.innerText = shuffleLineup[0].Answers[i]
         // }
-
-
-
 
 // function ansrLine(){
 //     for (let i=0; i < answers.length; i++){
